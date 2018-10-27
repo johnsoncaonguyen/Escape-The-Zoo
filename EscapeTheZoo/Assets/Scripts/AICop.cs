24ee2226de14ca7ddc459896c3de1e479e724ec6
vinayak gargya <vinayak4gargya@gmail.com>
2018-10-27 14:59:38 -0400

    Packaged cop Import /Packages/Cop2 into your scene

--- EscapeTheZoo/Assets/Scripts/AICop.cs
+++ EscapeTheZoo/Assets/Scripts/AICop.cs
@@ -6,17 +6,18 @@ using UnityEngine.AI;
 public class AICop : MonoBehaviour {
     public GameObject player;
     NavMeshAgent nav_mesh;
-    Animator anim;
+    Animation anim;
     VelocityReporter vReporter;
     public GameObject[] waypoints;
     int currWaypoint = -1;
     public enum AIStates { Patrol, Chase }
     AIStates AIstate;
+    public float chaseDistance;
     // Use this for initialization
     void Start()
     {
         nav_mesh = GetComponent<NavMeshAgent>();
-        anim = GetComponent<Animator>();
+        anim = GetComponent<Animation>();
         vReporter = player.GetComponent<VelocityReporter>();
 
         AIstate = AIStates.Patrol;
@@ -26,6 +27,8 @@ public class AICop : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        Vector3 distanceToPlayer = (player.transform.position - this.transform.position);
+        float dir = Vector3.Dot(this.transform.forward,distanceToPlayer);
         switch (AIstate)
         {
             case AIStates.Patrol:
@@ -39,10 +42,15 @@ public class AICop : MonoBehaviour {
                     else
                         SetNextWaypoint();
                 }
-                anim.SetFloat("vely", nav_mesh.velocity.magnitude / nav_mesh.speed);
+                if (distanceToPlayer.magnitude < chaseDistance && dir > 0)
+                    AIstate = AIStates.Chase;
+                if (!anim.isPlaying)
+                    anim.Play("Take 001");
 
                 break;
             case AIStates.Chase:
+                if (distanceToPlayer.magnitude > chaseDistance || dir < 0)
+                    AIstate = AIStates.Patrol;
                 if (!nav_mesh.pathPending && nav_mesh.remainingDistance < 1.5f)
                 {
                     AIstate = AIStates.Patrol;
@@ -51,9 +59,11 @@ public class AICop : MonoBehaviour {
                 else
                 {
                     ChaseWaypoint();
-                    anim.SetFloat("vely", nav_mesh.velocity.magnitude / nav_mesh.speed);
-
+                
                 }
+                if(!anim.isPlaying)
+                    anim.Play("Take 001");
+
                 break;
         }
 
