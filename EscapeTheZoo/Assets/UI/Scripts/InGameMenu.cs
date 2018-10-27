a86a6eab02c8cf28ae26bce765048a92fc6bc95e
Theodore Panagiotopoulos <theopanag7@gmail.com>
2018-10-27 02:17:27 -0400

    Fixed UI error in timer

---
+++ EscapeTheZoo/Assets/UI/Scripts/InGameMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour {
+
+	// Resume Game
+	public void ResumeGame () {
+		Time.timeScale = 1;
+	}
+
+	// Restart Game
+	public void RestartGame () {
+		Time.timeScale = 1;
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+
+	// Quit Game
+	public void QuitGame () {
+		Time.timeScale = 1;
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex - 1);
+	}
+
+}
---
+++ EscapeTheZoo/Assets/UI/Scripts/UIManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour {
+
+
+	public GameObject inGameMenu;
+	public GameObject inGameHUD;
+
+	public TextMeshProUGUI timerText;
+
+	private bool gameRunning;
+
+	// Use this for initialization
+	void Start () {
+		gameRunning = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CheckForEscape ();
+
+		UpdateTimer ();
+
+	}
+
+	void UpdateTimer(){
+
+		//Debug.Log ("Elapsed Time: " + Time.time);
+		int seconds = (int)(Time.time);
+		int minutes = seconds / 60;
+		seconds -= minutes * 60;
+
+		string secStr = (seconds >= 10) ? ""+seconds : "0" + seconds;
+		string minStr = (minutes >= 10) ? ""+minutes : "0" + minutes;
+
+
+		timerText.SetText (minStr+" : "+secStr);
+
+	}
+
+	void CheckForEscape(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Debug.Log ("Escape Pressed.");
+
+			if (gameRunning) {
+				Debug.Log ("Pausing Game and pulling up the in-game menu");
+				Time.timeScale = 0;
+			} else {
+				Debug.Log ("Resuming Game and hiding the in-game menu");
+				Time.timeScale = 1;
+			}
+
+			gameRunning = !gameRunning;
+			UpdateUI ();
+		}
+	}
+
+	void UpdateUI(){
+		if (gameRunning) {
+			inGameMenu.SetActive (false);
+			inGameHUD.SetActive (true);
+		} else {
+			inGameMenu.SetActive (true);
+			inGameHUD.SetActive (false);
+		}
+	}
+}
