aff8eecf73d89489252ef34ab6ccf433bc4330e3
Theodore Panagiotopoulos <theopanag7@gmail.com>
2018-10-28 19:50:59 -0400

    Cage Interactions

---
+++ EscapeTheZoo/Assets/BrokenVector/LowPolyFencePack/Scripts/GameInteractionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInteractionController : MonoBehaviour {
+
+	public BrokenVector.LowPolyFencePack.DoorController doorController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log ("Trigger Enter! "+collider.gameObject.tag);
+		if(collider.gameObject.tag == "Player") // this string is your newly created tag
+		{
+			doorController.OpenDoor ();
+		}
+	}
+}
