4cbe4d1c25e73a9fe0a77a88025ebe07aab0d8d6
Drew Teachout <drewteachout@gmail.com>
2018-10-28 14:15:05 -0400

    Added turning and camera follow

--- EscapeTheZoo/Assets/Scripts/CompleteCameraController.cs
+++ EscapeTheZoo/Assets/Scripts/CompleteCameraController.cs
@@ -6,7 +6,6 @@ public class CompleteCameraController : MonoBehaviour
 
     public GameObject player;       //Public variable to store a reference to the player game object
 
-
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
     // Use this for initialization
@@ -19,7 +18,14 @@ public class CompleteCameraController : MonoBehaviour
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        if (player == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        transform.position = player.transform.TransformPoint(offset / 2);
+        transform.LookAt(player.transform);
     }
 }
