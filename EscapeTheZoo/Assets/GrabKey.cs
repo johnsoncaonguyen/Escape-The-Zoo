95ba378a4b9b2be00e0eeecfa17d65766cceaeef
Vinayak Gargya <vinayak4gargya@gmail.com>
2018-11-17 00:13:02 -0500

    Included a key that can be picked up and used to open the door

---
+++ EscapeTheZoo/Assets/GrabKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabKey : MonoBehaviour {
+
+    public int nKeys = 0;
+	// Use this for initialization
+	void Start () {
+        nKeys = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void giveKey()
+    {
+        nKeys++;
+    }
+}
