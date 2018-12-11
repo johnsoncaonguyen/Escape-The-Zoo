978122a73769c8a191e952159675fddd946249c1
Theodore Panagiotopoulos <theopanag7@gmail.com>
2018-12-11 11:49:19 -0500

    Fixed Faces UI. It now updates when you rescue an animal

---
+++ EscapeTheZoo/Assets/RescueScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescueScript : MonoBehaviour {
+    Animation anim;
+    bool rescued = false;
+    
+	//public enum ANIMALS { HIPPO, LION, GIRAFFE, CROCODILE}
+    //public ANIMALS animal = ANIMALS.HIPPO;
+	public Prefs.Animals animal;
+	public Animal2DManager facesUI;
+
+	// Use this for initialization
+    float rescueTime;
+	void Start () {
+        anim = this.GetComponent<Animation>();
+        rescued = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(rescued)
+        {
+            if (rescueTime + 10 < Time.time)
+                this.transform.localScale = new Vector3(0f,0f,0f);
+        }
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        print("Collided with " + other);
+        if (other.gameObject.tag == "Player" & !rescued)
+        {
+            rescueTime = Time.time;
+            rescued = true;
+            ScoreSystem.getInstance().addToScore(100, ScoreSystem.scType.NONE);
+
+            switch (animal)
+            {
+			case Prefs.Animals.Hippo:
+                print("Rescued Hippo");
+                anim.Play("Success Hippo");
+                NotificationScreen.getInstance().displayNotification("Thanks for rescuing me! Here is something to help you. I was working on this in my time in the cage.", Time.time, 5);
+                break;
+			case Prefs.Animals.Croc:
+                print("Rescued Croc");
+                anim.Play("Success Crocodile");
+                NotificationScreen.getInstance().displayNotification("Thanks for rescuing me! Here is something to help you. I stole this from one of the guards.", Time.time, 5);
+                break;
+			case Prefs.Animals.Giraffe:
+                print("Rescued Hippo");
+                anim.Play("Success Giraffe");
+                NotificationScreen.getInstance().displayNotification("Thanks for rescuing me! I have nothing to give you.", Time.time, 5);
+                break;
+			case Prefs.Animals.Lion:
+                print("Rescued Lion");
+                anim.Play("Success Lion");
+                NotificationScreen.getInstance().displayNotification("ROAR! Thanks for rescuing me! Here is something to help you. It dropped out of the sky.", Time.time, 5);
+				break;
+			case Prefs.Animals.Elephant:
+				print("Rescued Elephant");
+				anim.Play("Success Elephant");
+				NotificationScreen.getInstance().displayNotification("Thanks for rescuing me! I have nothing to give you.", Time.time, 5);
+				break;
+            }
+
+			facesUI.RescuedAnimal (animal);
+        }
+    }
+}
--- EscapeTheZoo/Assets/Scripts/Animal2DManager.cs
+++ EscapeTheZoo/Assets/Scripts/Animal2DManager.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class Animal2DManager : MonoBehaviour {
 
-	public Image HippoFace, ElephantFace, CrocFace, GiraffeFace, LionFace;
+	public Image HippoFace, HippoFaceShaded, ElephantFace, ElephantFaceShaded;
+	public Image CrocFace, CrocFaceShaded, GiraffeFace, GiraffeFaceShaded, LionFace, LionFaceShaded;
 
-	private Image[] Faces = new Image[4];
+	private Image[] faces = new Image[8];
 
 	// Use this for initialization
 	void Start () {
@@ -19,40 +20,79 @@ public class Animal2DManager : MonoBehaviour {
 
 	}
 
-
-
 	void InitBasedOnSelectedAnimal(){
+		//faces = new Image[8]{ };
+		Image Face_i = HippoFace;
+		Image Face_i_shaded = HippoFaceShaded;
 		int index = 0;
 		for (int i = 0; i < 5; i++) {
-			Image Face_i = HippoFace;
 			switch (i) {
+			case 0:
+				Face_i = HippoFace;
+				Face_i_shaded = HippoFaceShaded;
+				break;
 			case 1:
 				Face_i = ElephantFace;
+				Face_i_shaded = ElephantFaceShaded;
 				break;
 			case 2:
 				Face_i = CrocFace;
+				Face_i_shaded = CrocFaceShaded;
 				break;
 			case 3:
 				Face_i = GiraffeFace;
+				Face_i_shaded = GiraffeFaceShaded;
 				break;
 			case 4:
 				Face_i = LionFace;
+				Face_i_shaded = LionFaceShaded;
 				break;
 			}
+			Face_i.gameObject.SetActive (false);
 
 			if ((Prefs.Animals)i == Prefs.ActiveAnimal) {
-				Face_i.gameObject.SetActive (false);
+				Face_i_shaded.gameObject.SetActive (false);
 				continue;
 			}
 
-			Faces [index] = Face_i;
+			faces [2*index] = Face_i;
+			faces [2*index+1] = Face_i_shaded;
 
 			index++;
 		}
 
-		for( int i = 0 ; i < Faces.Length ; i++){
-			RectTransform imgRect = Faces[i].GetComponent<RectTransform> ();
-			imgRect.anchoredPosition = new Vector2 (0, -i*60-30);
+		for( int i = 0 ; i < faces.Length ; i+=2){
+			RectTransform imgRect = faces[i].GetComponent<RectTransform> ();
+			imgRect.anchoredPosition = new Vector2 (0, -i/2*60-30);
+
+			imgRect = faces[i+1].GetComponent<RectTransform> ();
+			imgRect.anchoredPosition = new Vector2 (0, -i/2*60-30);
 		}
 	}
+
+	public void RescuedAnimal(Prefs.Animals animal){
+		switch (animal) {
+		case Prefs.Animals.Hippo:
+			HippoFace.gameObject.SetActive(true);
+			HippoFaceShaded.gameObject.SetActive(false);
+			break;
+		case Prefs.Animals.Elephant:
+			ElephantFace.gameObject.SetActive(true);
+			ElephantFaceShaded.gameObject.SetActive(false);
+			break;
+		case Prefs.Animals.Croc:
+			CrocFace.gameObject.SetActive(true);
+			CrocFaceShaded.gameObject.SetActive(false);
+			break;
+		case Prefs.Animals.Lion:
+			LionFace.gameObject.SetActive(true);
+			LionFaceShaded.gameObject.SetActive(false);
+			break;
+		case Prefs.Animals.Giraffe:
+			GiraffeFace.gameObject.SetActive(true);
+			GiraffeFaceShaded.gameObject.SetActive(false);
+			break;
+		}
+	}
+
 }
