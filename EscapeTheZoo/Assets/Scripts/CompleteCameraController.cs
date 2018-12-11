b3d59cdf8f305849a4779e7dd40d1e6e607a3312
Orestis.Mark <orestis.markozanes@gmail.com>
2018-12-11 13:39:34 -0500

    camera

--- EscapeTheZoo/Assets/Scripts/CompleteCameraController.cs
+++ EscapeTheZoo/Assets/Scripts/CompleteCameraController.cs
@@ -10,7 +10,7 @@ public class CompleteCameraController : MonoBehaviour
 	private Vector3 offset;
 	private float horizontal_angle;
 	private float vertical_angle;
-	private float turnSpeed = 2f;
+	public float turnSpeed = 2f;
 	private float cameraInertia = 1.05f;
 
 	void Start()
@@ -21,26 +21,27 @@ public class CompleteCameraController : MonoBehaviour
 
 	void LateUpdate()
 	{
-		//if (Input.GetMouseButton (1))// || Input.GetMouseButton (1)) 
-		//{
-		//	//horizontal_angle += Input.GetAxis ("Mouse X") * turnSpeed;
-		//	vertical_angle -= Input.GetAxis ("Mouse Y") * turnSpeed;
+        if (Input.GetMouseButton(1))// || Input.GetMouseButton (1)) 
+        {
 
-		//	vertical_angle = Mathf.Clamp (vertical_angle, 10-camera_height, 80 - camera_height);
-		//	//horizontal_angle = Mathf.Clamp (horizontal_angle, -180, 180);
-				
-		//	//Debug.Log ("Angles: " + horizontal_angle +", " + vertical_angle);
-		//}
-		//else
-		//{
-		//	horizontal_angle /= cameraInertia;
-	 //   	vertical_angle /= cameraInertia;
-		//}
+            horizontal_angle += Input.GetAxis("Mouse X") * turnSpeed;
+            vertical_angle -= Input.GetAxis("Mouse Y") * turnSpeed;
 
-        horizontal_angle += Input.GetAxis("Mouse X") * turnSpeed;
-        vertical_angle -= Input.GetAxis("Mouse Y") * turnSpeed;
+            vertical_angle = Mathf.Clamp(vertical_angle, 10 - camera_height, 80 - camera_height);
+            //horizontal_angle += Input.GetAxis ("Mouse X") * turnSpeed;
+            vertical_angle -= Input.GetAxis("Mouse Y") * turnSpeed;
 
-        vertical_angle = Mathf.Clamp(vertical_angle, 10 - camera_height, 80 - camera_height);
+            vertical_angle = Mathf.Clamp(vertical_angle, 10 - camera_height, 80 - camera_height);
+            //horizontal_angle = Mathf.Clamp (horizontal_angle, -180, 180);
+
+            //Debug.Log ("Angles: " + horizontal_angle +", " + vertical_angle);
+        }
+        else
+        {
+            horizontal_angle /= cameraInertia;
+            vertical_angle /= cameraInertia;
+        }
+
 
         Vector3 centerOfMass = player.transform.position + new Vector3 (0, 2, 0);
 
